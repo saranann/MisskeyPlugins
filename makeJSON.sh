b165127599148de0fd5984c802a95b1358a29882
saranann <116860189+saranann@users.noreply.github.com>
2024-01-11 00:38:13 +0900

    hash値の計算方法の修正

--- makeJSON.sh
+++ makeJSON.sh
@@ -10,13 +10,11 @@ plugin_code=$(cat $file)
 plugin_code=${plugin_code//$lf/\\\\n}
 plugin_code=${plugin_code//$double_quarto/\\$double_quarto}
 target_file=json/${file//src\//}.json
-echo $plugin_code > $target_file
-hash=$(sha512sum $target_file)
 echo "{
     \"type\": \"plugin\",
     \"data\": \"$plugin_code\"
 }" > $target_file
-
+hash=$(sha512sum $target_file)
 list="${list}https://{HOST}/install-extentions?url=https://saranann.github.io/MisskeyPlugins/$target_file&hash=$hash
 "
 done
